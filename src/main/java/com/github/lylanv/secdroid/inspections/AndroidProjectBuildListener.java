0f28c801ce9967f88a2fc0838b010186277ee638
Lyla NV <lyla.naghipour@gmail.com>
2024-10-24 16:32:37 +0200

    events added

---
+++ src/main/java/com/github/lylanv/secdroid/inspections/AndroidProjectBuildListener.java
@@ -0,0 +1,17 @@
+package com.github.lylanv.secdroid.inspections;
+
+import com.github.lylanv.secdroid.events.BuildSuccessEvent;
+import com.android.tools.idea.projectsystem.ProjectSystemBuildManager;
+import com.android.tools.idea.projectsystem.ProjectSystemBuildManager.BuildListener;
+
+public class AndroidProjectBuildListener implements BuildListener {
+
+    @Override
+    public void buildCompleted(final ProjectSystemBuildManager.BuildResult buildStatus) {
+        if (buildStatus.getStatus() == ProjectSystemBuildManager.BuildStatus.SUCCESS){
+            System.out.println("Build completed successfully");
+            EventBusManager.post(new BuildSuccessEvent(true));
+        }
+    }
+
+}
